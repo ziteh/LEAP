ad54282dece6c09a00aa9bceb7f0a7d8f25e0137
ZiTe-H <honmonoh@gmail.com>
2020-04-09 18:25:45 +0800

    修改ADC極限讀值

--- SG90_Testing_Model-01/src/main.c
+++ SG90_Testing_Model-01/src/main.c
@@ -32,23 +32,23 @@
 #define MESSAGE5   " program built with "
 #define MESSAGE6   " Atollic TrueSTUDIO "
 
-#define PosStr		(0.07)	// Leg straight
-#define PosBen		(3)	// Knee bent
+#define LimPos_Straight	(76)	// Leg straight
+#define LimPos_Bent		(3724)	// Knee bent
 //#define PosOffset	(0)
 
 /* Private macro */
 /* Private variables */
  USART_InitTypeDef USART_InitStructure;
  static __IO uint32_t TimingDelay;
- int POTdir=0; //0:Def; 1:Straight-MAX; -1:Bent-MAX
+ int POTdir=0; //0:Non-Limit; 1:Lim-Straight_Max; -1:Lim-Bent_Max
 
 /* Private function prototypes */
 /* Private functions */
  void Delay_normal(__IO u32 nTime);
  void Delay(__IO uint32_t nTime);
- float get_adc1();
+// float ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5);
  char* Number_TO_String(uint16_t Number);
- float get_adc1();
+// float ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5);
  void MotorCtrl(u16 TargetCCR);
 
 /**
@@ -69,98 +69,49 @@ int main(void)
 	PWM_Initialization();
 	NVIC_Initialization();
 
+	// User-LED
+	Pin_Clr(LD2);
+
+	// Motor
 //	MotorCtrl(530);
 	TIM_SetCompare2(TIM3, 530);
 
-	//int vPWM = 525; // 525～1720
-	//int dPWM = 0;
-/*
-	ADC_InitTypeDef ADC_InitStruct;
-
-//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);//IO时钟打开，ADC1时钟打开
-	RCC_ADCCLKConfig(RCC_PCLK2_Div6);//这个是对ADC时钟分频的，ADC时钟不能超过14m，
-
-	ADC_DeInit(ADC1);
-	ADC_InitStruct.ADC_ContinuousConvMode=DISABLE;//不需要连续转换
-	ADC_InitStruct.ADC_DataAlign=ADC_DataAlign_Right;//数据向右对齐
-	ADC_InitStruct.ADC_ExternalTrigConv=ADC_ExternalTrigConv_None;//不需要事件触发，软件触发就好
-	ADC_InitStruct.ADC_Mode=ADC_Mode_Independent;//独立模式
-	ADC_InitStruct.ADC_NbrOfChannel=1;//只转换一个通道
-	ADC_InitStruct.ADC_ScanConvMode=DISABLE;//只有一个通道，所以不需要浏览转换
-	ADC_Init(ADC1,&ADC_InitStruct);
-
-	ADC_Cmd(ADC1,ENABLE);//这是个坑，一定要使能ADC后才能校准，
-	ADC_ResetCalibration(ADC1);//复位校准
-	while(ADC_GetResetCalibrationStatus(ADC1)==1);//等待复位校准完成
-	ADC_StartCalibration(ADC1);//校准
-	while(ADC_GetCalibrationStatus(ADC1)==1);//等待校准完成
-*/
-
-	float Voltage=0;//电压值
-//	delay_init();	//延时函数初始化
-//	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
-//	init__uart1();//串口1初始化
-//	init_adc3();
-
-	Pin_Clr(LD2);
-
 	while(1)
 	{
-		Voltage=get_adc1();//获取ADC的电压值
-//		printf("Voltage=%f\r\n",Voltage);//发送到电脑
 		USART_Send(USART2, Number_TO_String(TIM3->CCR2));
-		USART_Send(USART2, "   ");
+		USART_Send(USART2, "  ");
 		USART_Send(USART2, Number_TO_String(POTdir));
-		USART_Send(USART2, "   ");
-		USART_Send(USART2, Number_TO_String(Voltage*10000));
+		USART_Send(USART2, "  ");
+		USART_Send(USART2, Number_TO_String(ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5)));
+		USART_Send(USART2, "\n");
 
-
-		USART_Send(USART2, "\n");//发送到电脑
-		Delay_normal(0xFFFF);	//这个延时只是为了让数据输出慢点，方便观察
+		Delay_normal(0xFFFF);
 	}
 }
 
 void MotorCtrl(u16 TargetCCR)
 {
-	/*
-	while((TargetCCR != (TIM3->CCR2)&(get_adc1() < PosStr)&(get_adc1() > PosBen)))
+	if((POTdir == 1)|(ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5) <= LimPos_Straight)) //Str-Max
 	{
-		u16 NowCCR = (TIM3->CCR2);
-		Pin_Set(LD2);
-		if(TargetCCR > NowCCR)
-		{
-			TIM_SetCompare2(TIM3, NowCCR+1);
-		}
-		else
-		{
-			TIM_SetCompare2(TIM3, NowCCR-1);
-		}
-
-		Delay_normal(0xf0f);
-	}
-	*/
-
-	if((POTdir == 1)|(get_adc1() <= PosStr)) //Str-Max
-	{
-		while((TargetCCR > (TIM3->CCR2)) & (get_adc1() < PosBen))
+		while((TargetCCR > (TIM3->CCR2)) & (ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5) < LimPos_Bent))
 		{
 			u16 NowCCR = (TIM3->CCR2);
 			TIM_SetCompare2(TIM3, NowCCR+1);
 			Delay_normal(0xf0f);
 		}
 	}
-	else if((POTdir == -1)|(get_adc1() >= PosBen)) //Bent-Max
+	else if((POTdir == -1)|(ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5) >= LimPos_Bent)) //Bent-Max
 	{
-		while((TargetCCR < (TIM3->CCR2)) & (get_adc1() > PosStr))
+		while((TargetCCR < (TIM3->CCR2)) & (ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5) > LimPos_Straight))
 		{
 			u16 NowCCR = (TIM3->CCR2);
 			TIM_SetCompare2(TIM3, NowCCR-1);
 			Delay_normal(0xf0f);
 		}
 	}
-	else	//((POTdir == 0))	// Def
+	else
 	{
-		while((TargetCCR != (TIM3->CCR2)) & (get_adc1() > PosStr) & (get_adc1() < PosBen))
+		while((TargetCCR != (TIM3->CCR2)) & (ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5) > LimPos_Straight) & (ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5) < LimPos_Bent))
 		{
 			u16 NowCCR = (TIM3->CCR2);
 			if(TargetCCR > NowCCR)
@@ -176,14 +127,12 @@ void MotorCtrl(u16 TargetCCR)
 		}
 	}
 
-//	if(!(get_adc1() > PosStr))
-	if(get_adc1() <= PosStr)
+	if(ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5) <= LimPos_Straight)
 	{
 		POTdir = 1;
 		USART_Send(USART2, "POS_Str-Max\n");
 	}
-//	else if(!(get_adc1() < PosBen))
-	else if(get_adc1() >= PosBen)
+	else if(ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5) >= LimPos_Bent)
 	{
 		POTdir = -1;
 		USART_Send(USART2, "POS_Bet-Max\n");
@@ -193,7 +142,6 @@ void MotorCtrl(u16 TargetCCR)
 		POTdir = 0;
 		USART_Send(USART2, "POS_Nor\n");
 	}
-//	Pin_Clr(LD2);
 }
 
 /**
@@ -208,17 +156,17 @@ char* Number_TO_String(uint16_t Number)
 	return string;
 }
 
-//获取ADC的电压值
-float get_adc1()
-{
-	float temp;
-	ADC_RegularChannelConfig(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5);//这里选择要转换的通道和转换时间
-	ADC_SoftwareStartConvCmd(ADC1,ENABLE);//软件触发转换
-	while(ADC_GetFlagStatus(ADC1,ADC_FLAG_EOC)==0);//等待转换完成
-	temp=ADC_GetConversionValue(ADC1);//获取ADC的值
-	temp=(3.3/4096)*temp;//转换为相应的电压，默认是12位转换的吧
-	return temp;
-}
+////获取ADC的电压值
+//float ADC_GetValue(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5)
+//{
+//	float temp;
+//	ADC_RegularChannelConfig(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5);//这里选择要转换的通道和转换时间
+//	ADC_SoftwareStartConvCmd(ADC1,ENABLE);//软件触发转换
+//	while(ADC_GetFlagStatus(ADC1,ADC_FLAG_EOC)==0);//等待转换完成
+//	temp=ADC_GetConversionValue(ADC1);//获取ADC的值
+//	temp=(3.3/4096)*temp;//转换为相应的电压，默认是12位转换的吧
+//	return temp;
+//}
 
 /**
  * @brief  Inserts a delay time(no-interrupt).

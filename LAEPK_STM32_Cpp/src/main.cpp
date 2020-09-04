f23d18f49607057157986b2c770e41ec92b8f45c
ZiTe <honmonoh@gmail.com>
2020-09-04 14:36:15 +0800

    Format

--- LAEPK_STM32_Cpp/src/main.cpp
+++ LAEPK_STM32_Cpp/src/main.cpp
@@ -77,6 +77,7 @@ void MotionHandler(void)
       //      USART_Send(USART2, "R: Fl-Start\r\n");
     }
     break;
+
   case Joint::Extensioning:
     if (NowJoint->ExtensionStopTriggered())
     {
@@ -114,6 +115,7 @@ void MotionHandler(void)
       //      USART_Send(USART2, "R: Fl-Start\r\n");
     }
     break;
+
   case Joint::Extensioning:
     if (NowJoint->ExtensionStopTriggered())
     {

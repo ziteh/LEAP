a2031cd918f99a64c8869d9a287deeec83d0ac31
ZiTe <honmonoh@gmail.com>
2020-09-10 23:58:06 +0800

    Enable Right leg

--- LAEPK_STM32_Cpp/src/main.cpp
+++ LAEPK_STM32_Cpp/src/main.cpp
@@ -22,7 +22,7 @@
 //  #define ENABLE_UNIT_TEST
 
 /* Uncomment/Comment the line below to enable/disable right or left leg. */
-//#define ENABLE_RIGHT_LEG
+#define ENABLE_RIGHT_LEG
 #define ENABLE_LEFT_LEG
 
 static __IO uint32_t TimingDelay;

8855b48ce461f103b89e738ed2263a6d18af2d5a
ZiTe <honmonoh@gmail.com>
2020-09-12 13:24:14 +0800

    加入手動RCC時鐘設定並註解

--- LAEPK_STM32_Cpp/src/main.cpp
+++ LAEPK_STM32_Cpp/src/main.cpp
@@ -124,8 +124,34 @@ void UpdateInfo(void)
 #endif
 }
 
+// FIXME Manual RCC 
 void RCC_Initialization(void)
 {
+  // RCC_DeInit();
+
+  // RCC_HSICmd(ENABLE);
+  // while (RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET)
+  // {
+  // }
+
+  // RCC_HCLKConfig(RCC_SYSCLK_Div1);
+  // RCC_PCLK1Config(RCC_HCLK_Div2);
+  // RCC_PCLK2Config(RCC_HCLK_Div1);
+
+  // FLASH_SetLatency(FLASH_Latency_2);
+  // FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+
+  // RCC_PLLConfig(RCC_PLLSource_HSE_Div2, RCC_PLLMul_16);
+  // RCC_PLLCmd(ENABLE);
+  // while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+  // {
+  // }
+
+  // RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+  // while (RCC_GetSYSCLKSource() != 0x08)
+  // {
+  // }
+
   RCC_GetClocksFreq(&RCC_Clocks);
 
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2 |
